556ace9601c3556c332469e70fe7fbf810a8daf5
adam <24186@ecam.bem>
2026-04-16 13:41:03 +0200

    structure de base pour la connection

--- conection.py
+++ conection.py
@@ -1,3 +1,22 @@
 import socket
 import struct
 import json 
+
+
+def register(nom, port, matricules): 
+    s= socket.socket() 
+    
+    data = json.dumps({
+            "request": "subscribe",
+            "port": port,
+            "nom": nom,
+            "matricules": matricules
+        }).encode() 
+    sent = s.send( data ) 
+    
+    if sent == len ( data ):
+     print (" Envoi complet ") 
+    
+    msg = s. recv (512) . decode ()
+    print ("Reçu" , len ( msg ) , "octets :")
+    print ( msg )
