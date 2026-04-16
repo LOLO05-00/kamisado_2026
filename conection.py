6203837d25b8467ac9cdf1162c640d0a115f559e
adam <24186@ecam.bem>
2026-04-16 14:25:11 +0200

    update et ajout de la longeur du message en binaire

--- conection.py
+++ conection.py
@@ -5,18 +5,28 @@ import json
 
 def register(nom, port, matricules): 
     s= socket.socket() 
+    s.connect(('172.17.83.160', 3000))
     
     data = json.dumps({
             "request": "subscribe",
             "port": port,
-            "nom": nom,
+            "name": nom,
             "matricules": matricules
         }).encode() 
-    sent = s.send( data ) 
     
-    if sent == len ( data ):
+    taille_en_b = struct.pack('I', len(data))
+    s.send(taille_en_b) 
+    sent = s.send(data)
+    
+    if sent == len (data): 
      print (" Envoi complet ") 
+    taille_reponse_b = s.recv(4)
+    taille_reponse = struct.unpack('I', taille_reponse_b)[0]
     
-    msg = s. recv (512) . decode ()
-    print ("Reçu" , len ( msg ) , "octets :")
-    print ( msg )
+    
+    msg = s.recv(taille_reponse).decode('utf-8')
+    print("Reçu")
+    print(msg)
+
+
+register("MonIA", 5000, ["24186"])     
